7f0414c11dcd078a4f3d4ffb78e9ddd5a49a8b88
iulianai57 <iulianai5@yahoo.com>
2024-08-30 22:22:16 +0300

    Parsed the JSON with the user info, and completed the HelloUser page.

--- my-app/src/pages/HelloUser.js
+++ my-app/src/pages/HelloUser.js
@@ -4,9 +4,42 @@ import GetCookie from "../hooks/getCookie";
 import getCookie from "../hooks/getCookie";
 
 export default function helloUser(){
-    const a = GetCookie('userCookie');
-    //console.log("CookieName = ", a);
+    const userInfoJson = GetCookie('userCookie');
+    var userInfoJsonParsed = JSON.parse(userInfoJson);
     return (
-        a
+        <Box>
+        <Box style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 40,
+            marginTop: '2rem',
+            color: '#008000',
+            fontWeight: 700,
+        }}>Hi {userInfoJsonParsed.username}</Box>
+    <Box
+        style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 40,
+            marginTop: '2rem',
+            color: '#008000',
+            fontWeight: 700,
+        }}>
+        Let's play!
+    </Box>
+    <Box style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '0.6rem 1.2rem',
+        border: 'none',
+    }}>
+        <button type="submit" className="btn btn-outline-primary">
+            Go to the games ->
+        </button>
+    </Box>
+        </Box>
     )
 }
